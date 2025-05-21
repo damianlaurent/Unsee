3f3990744bfab701e2231cb819c0d3ede5c5533e
damienlaurent <damienylaurent@gmail.com>
2025-05-20 23:05:33 -0500

    .

--- src/main/java/net/damian/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/damian/tutorialmod/block/ModBlocks.java
@@ -6,6 +6,7 @@ import net.minecraft.client.renderer.block.BlockRenderDispatcher;
 import net.minecraft.client.resources.model.Material;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.Display;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.FireworkRocketItem;
 import net.minecraft.world.item.Item;
@@ -28,6 +29,7 @@ import java.awt.*;
 import java.util.function.Supplier;
 
 public class ModBlocks {
+
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, unsee.MOD_ID);
 
@@ -46,8 +48,6 @@ public class ModBlocks {
                     .lightLevel(state -> 0).noCollission()
                     .isRedstoneConductor((state, world, pos) -> false));
         }
-
-
     }
 
     private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block) {
@@ -57,5 +57,6 @@ public class ModBlocks {
     public static void register(IEventBus eventBus) {
         BLOCKS.register(eventBus);
     }
+
 
 }
